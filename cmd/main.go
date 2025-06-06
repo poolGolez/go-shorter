345479fee95a581534ab0e3c24873dc9fc581f08
Pool Golez <loop.edward@gmail.com>
2025-06-06 11:37:24 +0800

    Add .env setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"log"
+
+	"example.com/go-shorter/internals/gorm"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadEnv()
 	server := gin.Default()
-	InitDb()
+	initDb()
 
 	server.POST("/urls", ShortenUrl)
 	server.GET("/:code", RedirectToTarget)
 
 	server.Run()
+}
+
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on environment variables")
+	} else {
+		log.Println("A .env file was loaded")
+	}
+}
+
+func initDb() {
+	gorm.DbConnect()
+	migrateDb()
 }
